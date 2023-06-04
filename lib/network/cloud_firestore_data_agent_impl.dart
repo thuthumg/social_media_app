d62960e2f5cdc26214502e5d6240663381b940a4
Thu Thu <ms.jellyfishgirl@gmail.com>
2023-06-04 10:22:16 +0630

    setup create data function and delete data function at cloud firestore

--- lib/network/cloud_firestore_data_agent_impl.dart
+++ lib/network/cloud_firestore_data_agent_impl.dart
@@ -1,23 +1,42 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:social_media_app/data/vos/news_feed_vo.dart';
 import 'package:social_media_app/network/social_data_agent.dart';
 
+///News Feed Collection
+const newsFeedCollection = "newsfeed";
 class CloudFirestoreDataAgentImpl extends SocialDataAgent{
+
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+
   @override
   Future<void> addNewPost(NewsFeedVO newPost) {
-    // TODO: implement addNewPost
-    throw UnimplementedError();
+    return _firestore
+        .collection(newsFeedCollection)
+        .doc(newPost.id.toString())
+        .set(newPost.toJson());
   }
 
   @override
   Future<void> deletePost(int postId) {
-    // TODO: implement deletePost
-    throw UnimplementedError();
+   return _firestore
+       .collection(newsFeedCollection)
+       .doc(postId.toString())
+       .delete();
   }
 
   @override
   Stream<List<NewsFeedVO>> getNewsFeed() {
-    // TODO: implement getNewsFeed
-    throw UnimplementedError();
+    //snapshots => querySnapShot=> querySnapShot.docs => List<QueryDocumentSnapshot> => data()=> List<Map<String, dynamic>> => NewsFeedVO.fromJson => List<NewsFeedVO>
+    return _firestore
+        .collection(newsFeedCollection)
+        .snapshots()
+        .map((querySnapShot) {
+          return querySnapShot.docs.map<NewsFeedVO>((document){
+            return NewsFeedVO.fromJson(document.data());
+          }).toList();
+    });
+
   }
 
   @override
